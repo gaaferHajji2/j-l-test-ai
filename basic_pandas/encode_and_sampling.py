cb5116d9dbe7d56b6cdce9e2d87397764fb88a45
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-11 06:21:54 +0300

    Init encode and sampling data example

---
+++ basic_pandas/encode_and_sampling.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    'j1': ['jl-01', 'jl-02', 'jl-03', 'jl-01'],
+    'j2': ['jl-03', 'jl-04', 'jl-05', 'jl-06'],
+    'j3': ['jl-06', 'jl-05', 'jl-06', 'jl-07']
+})
