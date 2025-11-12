47504b4012c5b2e82b2c8a67f1aa698f3dd29813
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-12 03:06:19 +0300

    Draw simple arrowed line

--- Image-Processing-01/draw_shapes/simple_arrowed_line.py
+++ Image-Processing-01/draw_shapes/simple_arrowed_line.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 simple_img = np.zeros((256, 256, 3), np.uint8)
-cv2.line(simple_img, (0, 0), (255, 255), (0, 0, 255), 15)
+cv2.arrowedLine(simple_img, (215, 200), (75, 200), (0, 0, 255), 3)
 cv2.imshow('Image', simple_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
