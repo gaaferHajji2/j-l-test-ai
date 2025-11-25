ead718ac8e412d976fce2c025f5518330a79f1a0
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-25 15:24:09 +0300

    Refactor

--- Image-Processing-01/camera_basic/test_01.py
+++ Image-Processing-01/camera_basic/test_01.py
@@ -1,11 +1,11 @@
 import cv2
 
-print(cv2.__version__)
+# print(cv2.__version__)
 
 img = cv2.imread('./tomato.jpg')
 
-cv2.imshow('Image', img); # type: ignore
+cv2.imshow('Image', img) # type: ignore
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-cv2.imwrite('tomato-02.png', img); # type: ignore
+cv2.imwrite('tomato-02.png', img) # type: ignore
