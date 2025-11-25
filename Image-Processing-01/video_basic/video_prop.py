b4c390c21c14e3c9541467a69d0e9a77313fd076
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-25 16:03:09 +0300

    Finish from video property

--- Image-Processing-01/video_basic/video_prop.py
+++ Image-Processing-01/video_basic/video_prop.py
@@ -1,13 +1,29 @@
 import cv2
 
 camera = cv2.VideoCapture(0)
-
 # define the camera resolution
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
-
 # define the brightness of camera
 camera.set(cv2.CAP_PROP_BRIGHTNESS, 80)
-
 # define the contrast of camera
 camera.set(cv2.CAP_PROP_CONTRAST, 55)
+# print the values
+print("width: ", camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+print("height: ", camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print("brightness: ", camera.get(cv2.CAP_PROP_BRIGHTNESS))
+print("Sharpness: ", camera.get(cv2.CAP_PROP_SHARPNESS))
+# get the frames
+while True:
+    ret, frame = camera.read()
+    if not ret:
+        print("no ret found")
+        break
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        # Check if window is closed
+    if cv2.getWindowProperty('frame', cv2.WND_PROP_VISIBLE) < 1:
+        break
+camera.release()
+cv2.destroyAllWindows()
