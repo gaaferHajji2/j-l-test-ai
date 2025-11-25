b815cce027aaf03bc4c86724de18d4d6fdb678d6
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-25 15:23:13 +0300

    Change requirements of image processing only & test it

--- Image-Processing-01/camera_basic/test_01.py
+++ Image-Processing-01/camera_basic/test_01.py
@@ -2,10 +2,10 @@ import cv2
 
 print(cv2.__version__)
 
-img = cv2.imread('./QR.png')
+img = cv2.imread('./tomato.jpg')
 
 cv2.imshow('Image', img); # type: ignore
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-cv2.imwrite('Qr-02.png', img); # type: ignore
+cv2.imwrite('tomato-02.png', img); # type: ignore
