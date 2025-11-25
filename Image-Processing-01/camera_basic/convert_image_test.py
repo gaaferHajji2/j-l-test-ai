7038dc2fb3f89823b3acb7229af39e8d98d458e0
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-25 15:30:18 +0300

    Refactor & save converted image

--- Image-Processing-01/camera_basic/convert_image_test.py
+++ Image-Processing-01/camera_basic/convert_image_test.py
@@ -3,9 +3,8 @@ import cv2
 img = cv2.imread('tomato.jpg')
 cv2.imshow('Image', img); # type: ignore
 cv2.waitKey(0)
-
 img_convert = cv2.cvtColor(img, cv2.COLOR_BGR2RGB); # type: ignore
 cv2.imshow('Image', img_convert)
 cv2.waitKey(0)
-
+cv2.imwrite('cvt-image.jpg', img_convert)
 cv2.destroyAllWindows()
